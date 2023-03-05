712279d2c009c1bff18847b658edb0a99b945af2
Farstron <109409135+Farstron@users.noreply.github.com>
2023-03-05 23:23:27 +0700

    add Products

---
+++ HW1/HW1.java
@@ -0,0 +1,11 @@
+package HW1;
+
+/**
+ * HW1
+ */
+public class HW1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ HW1/Products.java
@@ -0,0 +1,24 @@
+package HW1;
+
+public class Products {
+    protected String name;
+    protected int price;
+    protected int count;
+    protected String unitOfMeasurement;
+    /**
+     * Конструктор Продуктов    
+     * @param name Наименование продукта
+     * @param price Цена продукта
+     * @param count Количество продукта
+     * @param unitOfMeasurement Единица измерения
+     */
+    public Products (String name, int price, int count, String unitOfMeasurement){
+        this.name = name;
+        this.price = price;
+        this.count = count;
+        this.unitOfMeasurement = unitOfMeasurement;
+    }
+    public String getInfo(){
+        return String.format("Наименование: %s;\nЦена: %d;\nКоличество: %d;\nЕдиница измерения: %s;", name, price, count, unitOfMeasurement);
+    }
+}
