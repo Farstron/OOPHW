faf1ae70e771eb4d396d8e59bdd79eef9c310124
Farstron <109409135+Farstron@users.noreply.github.com>
2023-03-09 21:02:29 +0700

    add class Pet

---
+++ HW2/Pet.java
@@ -0,0 +1,10 @@
+package HW2;
+
+abstract class Pet implements Petable{
+    String nickName;
+    String breed;
+    boolean vaccinations;
+    String color;
+    String birthday;
+}
+
---
+++ HW2/Petable.java
@@ -0,0 +1,5 @@
+package HW2;
+
+public interface Petable{
+    String TakeCaress();
+}
