1820ea5840a3e2c9ff27b066f33c2ab38d0bc668
Farstron <109409135+Farstron@users.noreply.github.com>
2023-03-19 13:08:27 +0700

    all methods at complete (HW4)

--- HW4/HW4.java
+++ HW4/HW4.java
@@ -2,15 +2,32 @@ package HW4;
 
 public class HW4 {
     public static void main(String[] args) {
-        String[] ar = {"hello","af", "hello","af", "hello","af"};
+        String[] ar = {"START","hello","af", "hello","af", "hello","af","FINISH"};
+        String[] ar2 = {"|START|","-----","-----", "-----","|FINISH|"};
         MyList<String> myList = new MyList<String>(ar);
         myList.printList();
-        myList.add("world");
+        myList.add("WORLD");
         myList.printList();
-        myList.addByIndex("argtb", -3);
+        myList.addByIndex("BYINDEX", 0);
+        myList.addByIndex("BYINDEX", 3);
+        myList.addByIndex("BYINDEX", 100);
+        myList.addByIndex("BYINDEX", -3);
         myList.printList();
+        myList.add(ar2);
+        myList.printList();
+        myList.addByIndex(ar2, 0);
+        myList.addByIndex(ar2, 3);
+        myList.addByIndex(ar2, 100);
+        myList.addByIndex(ar2, -3);
+        myList.printList();
+
+
+
+
+
+
+
+        
         myList.length();
-        myList.add(ar);
-        myList.printList();
     }
 }
--- HW4/MyList.java
+++ HW4/MyList.java
@@ -1,17 +1,25 @@
 package HW4;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class MyList<T> {
+    /**
+     * 
+     */
     transient Object[] listData;
     private static final Object[] defaultListData = {};
     private static final Object[] emptyListData = {};
     private int size;
+
+    /**
+     * 
+     */
     public MyList(){
         this.listData = this.defaultListData;
         this.size = 0;
     }
+    /**
+     * @param initSize
+     */
     public MyList(int initSize){
         if (initSize >= 0){
             this.listData = new Object[initSize];
@@ -24,6 +32,9 @@ public class MyList<T> {
         }
         
     }
+    /**
+     * @param initCollection
+     */
     public MyList(T[] initCollection){
         this.size = initCollection.length;
         this.listData = initCollection;
@@ -33,21 +44,39 @@ public class MyList<T> {
         this.listData = new Object[1];
         listData[0] = initElement;
     }
+
+    /**Выод массива в консоль*/
     public void printList(){
         for (Object e : listData){
             System.out.print(e+" ");
         }
         System.out.println();
     }
+    // ADD block
+    /** Добавление элемента в конец массива
+     * @param initElement экземпляр класса T
+     */
     public void add(T initElement){
         this.size++;
         listData = Arrays.copyOf(listData, size);
         listData[size-1] = initElement;
     }
+    /** Добавление коллекции в конец массива
+     * @param initCollection экземпляр-коллекция класса T
+     */
     public void add(T[] initCollection){
-        this.size +=initCollection.length;
-        merge(listData, initCollection);
+        this.listData = mergeCollection(listData, initCollection);
     }
+    /** Добавление элемента по индексу.
+     * <pre>
+     * Правила добавления:
+     * Индекс > длины массива -> элемент добавляется в конец массива
+     * Индекс < 0 -> элемент добавляется с конца массива по индексу (длина массива + индекс)
+     * Индекс в пределах длины массива -> элемент добавляется по индексу
+     * </pre>
+     * @param initElement экземпляр класса T
+     * @param index индекс в массиве
+     */
     public void addByIndex (T initElement, int index){
         if (index > size) {
             add(initElement);
@@ -56,11 +85,35 @@ public class MyList<T> {
             if (index < 0) index = size+index;
             Object[] temp = Arrays.copyOfRange(listData,index,this.size);
             listData[index] = initElement;
-            listData = Arrays.copyOf(listData, size+1);
+            grow(1);
             merge(Arrays.copyOfRange(listData,0,index+1), temp);
         }
-
     }
+    /** Добавление коллекции по индексу. Вставка происходит по первому элементу коллекции.
+     * <pre>
+     * Правила добавления:
+     * Индекс > длины массива -> коллекция добавляется в конец массива
+     * Индекс < 0 -> коллекция  добавляется с конца массива по индексу (длина массива + индекс)
+     * Индекс в пределах длины массива -> коллекция  добавляется по индексу
+     * </pre>
+     * @param initCollection экземпляр-коллекция класса T
+     * @param index индекс в массиве
+     */
+    public void addByIndex(T[] initCollection, int index){
+        if (index > size) {
+            add(initCollection);
+        }
+        else{ 
+            if (index < 0) index = size+index;
+            Object[] temp = Arrays.copyOfRange(listData,index,this.size);
+            grow(initCollection.length);
+            this.listData = mergeCollection(mergeCollection(Arrays.copyOfRange(listData,0,index),initCollection), temp);
+        }
+    }
+    /**
+     * @param array1
+     * @param array2
+     */
     public void merge(Object[] array1, Object[] array2){
         for (int i= 0; i < array1.length;i++){
             this.listData[i] = array1[i];
@@ -70,9 +123,30 @@ public class MyList<T> {
         }
         this.size = array1.length + array2.length;
     }
+
+    /** Слияние коллекций
+     * @param array1 коллекция 1
+     * @param array2 коллекция 2
+     * @return коллекция, образованная из двух исходных
+     */
+    public Object[] mergeCollection(Object[] array1, Object[] array2){
+        Object[] resCollection = new Object[array1.length+array2.length];
+        for (int i= 0; i < array1.length;i++){
+            resCollection[i] = array1[i];
+        }
+        for (int i = 0; i < array2.length; i++){
+            resCollection[i+array1.length] = array2[i];
+        }
+        this.size = array1.length + array2.length;
+        return resCollection;
+    }
+    /** Увеличение длины массива на указанную величину
+     * @param up величина на которую нужно удлинить массив
+     */
     private void grow(int up){
         listData = Arrays.copyOf(listData, size+up);
     }
+    /** Вывод в консоль длины массива*/
     public void length(){
         System.out.println(listData.length);
     }
